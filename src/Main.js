f4fc068ab1efeab7a359aa71fc3fba0cde304314
vermad843 <vishnuverma7524@gmail.com>
2021-05-26 03:09:46 +0530

    main component

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './main.css';
+
+function Main() {
+    return (
+        <div className = "main">
+            <h1>
+                This is a main component
+            </h1>
+        </div>
+    )
+}
+
+export default Main
