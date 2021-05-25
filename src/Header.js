8ba48be8e89f15530b3e79bbd82f9d42d9acb76d
vermad843 <vishnuverma7524@gmail.com>
2021-05-25 22:37:23 +0530

    header__mid__content

--- src/Header.js
+++ src/Header.js
@@ -5,13 +5,24 @@ function Header() {
     return (
         <div className = "header">
             <div className = "header__left">
-               <h1>FICTION BEATS</h1>
+               <h2>FICTION BEATS</h2>
             </div>
             <div className = "header__mid">
-               <h1>FICTION BEATS</h1>
+               <div className = "header__mid__content">
+                 <h2>HOME</h2>
+                </div> 
+                <div className = "header__mid__content">
+                   <h2>DRUM& SAMPLE KITS</h2>     
+                </div>
+                <div className = "header__mid__content">
+                  <h2>CONTACT</h2>
+                </div>
+                <div className = "header__mid__content">
+                  <h2>FREE DOWNLOADS</h2>
+                </div>
             </div>
             <div className = "header__right">
-               <h1>FICTION BEATS</h1>
+               <h2>FICTION BEATS</h2>
             </div>
         </div>
     )
