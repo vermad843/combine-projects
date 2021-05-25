926d22b5591e0466734deb07d298547e6218e2dd
vermad843 <vishnuverma7524@gmail.com>
2021-05-26 03:14:03 +0530

    footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="app">
         <Header/>
         <Main/>
+        <Footer/>
     </div>
   );
 }
