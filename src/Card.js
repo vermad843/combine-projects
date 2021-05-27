891408e69c4246728413bbd69f1f5bba83bcc604
vermad843 <vishnuverma7524@gmail.com>
2021-05-28 03:54:40 +0530

    product component

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Card.css';
+import Product from './Product';
 
 function Card() {
     return (
         <div className = "card">
-            <h1>I am a Card Component</h1>            
+            <div className = "card__row">
+               <Product
+                    id = '12321341'
+                    title = "AudioTechnica"
+                    price = {11300}
+                    rating = {3}
+                    image = "AudioTechnica.jpg"
+                />
+                <Product
+                    id = '12321341'
+                    title = "AudioTechnica"
+                    price = {11300}
+                    rating = {3}
+                    image = "AudioTechnica.jpg"
+                />
+                <Product
+                    id = '12321341'
+                    title = "AudioTechnica"
+                    price = {11300}
+                    rating = {3}
+                    image = "AudioTechnica.jpg"
+                />
+                <Product
+                    id = '12321341'
+                    title = "AudioTechnica"
+                    price = {11300}
+                    rating = {3}
+                    image = "AudioTechnica.jpg"
+                />
+            </div>
         </div>
     )
 }
