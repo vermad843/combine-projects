f794bbd3472b793c70d538f1bb5339790ec710ce
vermad843 <vishnuverma7524@gmail.com>
2021-06-01 02:44:53 +0530

    drums__header

--- src/Drums.js
+++ src/Drums.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Drums.css'
 
 function Drums() {
     return (
         <div className = "drums">
-            <h1>I am a Drum&Sample Component</h1>
+            <h1>Shop</h1>
+            <div className = "drums__header">
+               <h3>Filter By</h3>
+               <h3>All Products</h3>
+               <h3>15 Products</h3>
+            </div>
         </div>
     )
 }
